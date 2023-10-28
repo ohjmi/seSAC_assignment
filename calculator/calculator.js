c9dd27c878b5b701e4350284630a591af4063bb0
ohjmi <ohjmi@naver.com>
2023-10-28 17:31:50 +0900

    계산기 만들기 과정 1-2

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,33 @@
-function button(value) {
-   document.getElementById('show').value += value;
-   console.log(button);
+// function button(i) {
+//    document.getElementById('show').value += i;
+//    console.log(i);
+// }
+
+// function button() {
+//    document.getElementById('show')
+// }
+
+// 함수 이름(7){
+//    레이블 
+// }
+
+// function button(button) {
+//    let show = document.getElementById('show');
+//    show.value = show.value += button;
+// }
+
+// function result() {
+//    let show = document.getElementById('show');
+//    let result = eval(show.value);
+//    document.getElementById('result').value = result;
+// }
+
+// function clear() {
+//    document.getElementById('show').value = "";
+//    document.getElementById('result').value = "";
+
+// }
+
+function button(i) {
+   console.log(i +1);
 }
