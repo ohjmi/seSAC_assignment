8d95b744ed461b9014b55562dcffb0224f305eaa
ohjmi <ohjmi@naver.com>
2023-10-30 20:52:00 +0900

    계산기 만들기 완료

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,18 +1,12 @@
-// function button(i) {
-//    document.getElementById('show').value += i;
-//    console.log(i);
-// }
-
 function button(num) {
    document.getElementById('show').value += num;
    console.log(num);
 }
 function calc() {
    let show = document.getElementById('show').value;
-   let result = eval(show.value); 
-   document.getElementById('result').value = result;
+   let result = eval(show); 
+   document.getElementById('show').value = result;
 }
 function clear() {
    document.getElementById('show').value = "";
-   document.getElementById('result').value = "";
 }
