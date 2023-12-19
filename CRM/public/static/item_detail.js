5de69356e90b4ef2c876af098213379875bca779
ohjmi <ohjmi@naver.com>
2023-12-19 21:10:45 +0900

    item 주문횟수 그래프 구현, y축에 표시되는 화면을 왼쪽에는 금액 오른쪽에는 횟수 숫자를 넣으려했으나 왼쪽으로만 몰려서 표시가 됨....

--- CRM/public/static/item_detail.js
+++ CRM/public/static/item_detail.js
@@ -12,15 +12,6 @@ function fetchItemData() {
     .then(data => {
       displayItem(data.item);
       displayItemMonth(data.monthItem); 
-
-      // drawChart(
-      //   data.map(item => item.month),       // labels
-      //   data.map(item => item.totalRevenue), // barData
-      //   data.map(item => item.count)         // lineData
-      // );
-      // const test = drawChart();
-      // console.log('메롱메롱:',test);
-        
     })
     .catch(error => {
       console.error('Error fetching user data:', error);
@@ -40,26 +31,10 @@ function displayItem(items) {
   });
 }
 
-
-// function displayItemMonth(monthItems) {
-//   const itemMonthTableBody = document.getElementById('itemMonthBody');
-//   itemMonthTableBody.innerHTML = '';
-//   monthItems.forEach(monthItem => {
-//     const row = document.createElement('tr');
-//     row.innerHTML = `
-//       <td>${monthItem.month}</td>
-//       <td>${monthItem.totalRevenue}</td>
-//       <td>${monthItem.count}</td>
-//     `;
-//     itemMonthTableBody.appendChild(row);
-//     drawChart(monthItem.month, monthItem.totalRevenue, monthItem.count);
-//   });
-// }
 function displayItemMonth(monthItems) {
   const itemMonthTableBody = document.getElementById('itemMonthBody');
   itemMonthTableBody.innerHTML = '';
 
-  // 배열을 초기화합니다.
   const chartData = {
     labels: [],
     barData: [],
@@ -86,58 +61,15 @@ function displayItemMonth(monthItems) {
 
 }
 
-
-// drawChart = (labels, barData, lineData) => {
-//   const ctx = document.getElementById('chart_wrap').getContext('2d');
-
-// var cc = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//       labels: labels,
-//       datasets: [
-//         {
-//           type: 'bar',
-//           label: '총 수익',
-//           data: barData,
-//           backgroundColor: 'rgba(75, 192, 192, 0.2)',
-//           borderColor: 'rgba(75, 192, 192, 1)',
-//           borderWidth: 1,
-//         },
-//         {
-//           type: 'line',
-//           label: '주문 횟수',
-//           data: lineData,
-//           borderColor: 'rgba(255, 99, 132, 1)',
-//           borderWidth: 1,
-//           fill: false,
-//         },
-//       ],
-//     },
-//     options: {
-//       scales: {
-//         x: {
-//           type: 'category',
-//           labels: labels,
-//         },
-//         y: {
-//           beginAtZero: true,
-//         },
-//       },
-//     },
-//   });
-// };
-
-// drawChart([],[],[]);
-
 // 차트를 생성하고 업데이트하는 함수
 function drawChart(labels, barData, lineData) {
-  console.log(lineData);
+
   // 이전 차트 인스턴스를 확인하고 제거
   if (window.myChart) {
     window.myChart.destroy();
   }
 
-  const ctx = document.getElementById('chart_wrap').getContext('2d');
+  const ctx = document.getElementById('chart').getContext('2d');
 
   // 새로운 차트 인스턴스 생성
   window.myChart = new Chart(ctx, {
@@ -147,32 +79,49 @@ function drawChart(labels, barData, lineData) {
       datasets: [
         {
           type: 'bar',
-          label: '총 수익',
+          label: '월 매출액',
           data: barData,
           backgroundColor: 'rgba(75, 192, 192, 0.2)',
           borderColor: 'rgba(75, 192, 192, 1)',
           borderWidth: 1,
+          yAxisID: 'yaxis1',
         },
         {
           type: 'line',
           label: '주문 횟수',
           data: lineData,
           borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1,
+          borderWidth: 3,
           fill: false,
+          order: 1,
+          yAxisID: 'yaxis2',
         },
       ],
     },
     options: {
       scales: {
+        y: [
+          {
+            type: 'linear',
+            position: 'left',
+            id: 'yaxis1',
+          },
+          {
+            type: 'linear',
+            position: 'right',
+            id: 'yaxis2',
+            grid: {
+              drawOnChartArea: false,
+            },
+          },
+        ],
         x: {
           type: 'category',
           labels: labels,
         },
-        y: {
-          beginAtZero: true,
-        },
       },
     },
+
+
   });
 }
